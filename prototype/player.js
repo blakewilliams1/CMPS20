d2d46edcd858d8c96d7c058365dd9f03fa4c4b71
blakewilliams1 <blauwill@ucsc.edu>
2015-02-10 18:34:02 -0800

    Update player.js
    added hide/unhide options. Game now requires hide-able objects. Beginning work on hide-able objects

--- prototype/player.js
+++ prototype/player.js
@@ -1,25 +1,44 @@
 function Player(){
   this.image;
   this.direction = "none";
-
+  this.hiding=false;
+  this.objectBehind;
   this.update = function(image){
-     if(this.image != image) return;
-     switch (this.direction){
-    case "right":
-      this.image.position.x += 4;
-      break;
+	if(this.image != image) return;
+	  switch (this.direction){
+	    case "right":
+		this.image.position.x += 4;
+		break;
 
-    case "left":
-      this.image.position.x -= 4;
-      break;
+		case "left":
+		this.image.position.x -= 4;
+		break;
 
-    case "up":
-      this.image.position.y -= 4;
-      break;
+		case "up":
+		this.image.position.y -= 4;
+		break;
 
-    case "down":
-      this.image.position.y += 4;
-      break;
+		case "down":
+		this.image.position.y += 4;
+		break;
+	}
   }
+  this.hide = function(hidingObject){
+    if(!hidingObject.occupied){
+      this.hiding=true;
+	  this.objectBehind=hidingObject;
+      //change 'game' to whatever we all the game instance
+      game.score+=hidingObject.points;
+	  //change sprite
+	  hidingObject.occupied=true;
+    }
+  }
+  this.unhide = function(){
+    if(this.hiding){
+	  this.hiding=false;
+	  game.score-=objectBehind.points;
+	  this.objectBehind=null;
+	  //change sprite
+	}
   }
 }
