7abd23f22834f6b417de236e9b4ce3ad965d89a8
blakewilliams1 <blauwill@ucsc.edu>
2015-03-08 18:16:07 -0700

    Almost finished level one
    Also renamed the well occupied texture
    Note: We still need to fix the way it checks if you're close enough to a
    hiding spot to hide it. It doesn't take the textures width into account,
    so larger spots like the well are unhidable

--- prototype/buildings.js
+++ prototype/buildings.js
@@ -1,5 +1,5 @@
 function Building(x, y) {
-	var texture = PIXI.Texture.fromImage("../Art Assets/png/smallBuilding.png");
+	var texture = PIXI.Texture.fromImage("../Art Assets/png/"+"Building.png");
 	var sprite = new PIXI.Sprite(texture);
 	sprite.anchor.x = .5;
 	sprite.anchor.y = .5;
